59780bb46d1f50d7ef590f48f115adf25e04c7bf
nivleM-ed <melvinedward98@gmail.com>
2020-06-04 14:43:46 +0800

    Working sample

---
+++ main.m
@@ -0,0 +1,24 @@
+faceDatabase = imageSet('FaceDatabaseATT','recursive');
+[training,test] = partition(faceDatabase,[0.8 0.2]);
+trainingFeatures = zeros(size(training,2)*training(1).Count,4680);
+featureCount = 1;
+for i=1:size(training,2)
+    for j = 1:training(i).Count
+        trainingFeatures(featureCount,:) = extractHOGFeatures(read(training(i),j));
+        trainingLabel{featureCount} = training(i).Description;    
+        featureCount = featureCount + 1;
+    end
+    personIndex{i} = training(i).Description;
+end
+faceClassifier = fitcecoc(trainingFeatures,trainingLabel);
+
+%Test for first face
+person = 1; %change from 1 to 40
+queryImage = read(test(person),1);
+queryFeatures = extractHOGFeatures(queryImage);
+personLabel = predict(faceClassifier,queryFeatures);
+% Map back to training set to find identity 
+booleanIndex = strcmp(personLabel, personIndex);
+integerIndex = find(booleanIndex);
+subplot(1,2,1);imshow(queryImage);title('Query Face');
+subplot(1,2,2);imshow(read(training(integerIndex),1));title('Matched Class');
